1d32f07086e5fa4d6e8e4a667bdb20730bedfb17
Naveena Marouthu <n_marouthu@apple.com>
2024-06-19 21:59:00 -0700

    With Work & Education details

--- src/components/page-components/Education/index.js
+++ src/components/page-components/Education/index.js
@@ -3,10 +3,33 @@ import Section from '../../base/Section'
 import './index.css';
 
 const Education = (props) => {
+    const timelineEntries = [
+        {"timeline": "2005-2009", "title": "Bachelors Degree", "location": "Bachelors in Computer Science & Engineering, JNT University, Kakinada, India", "description": "Some description"},
+    ]
+
     return (
         <Section {...props}>
-            <p>Education Section</p>
+            <div className="section-title">
+                <h2>Education</h2>
+            </div>
+            <div className="education-timeline">
+                <ul className="timeline">
+                    { timelineEntries && timelineEntries.map(timelineEntry => (
+                        <li key={timelineEntry.timeline}>
+                            <div className="timeline-info">{timelineEntry.timeline}</div>
+                            <div className="timeline-panel">
+                                <div className="heading">
+                                    <h3>{timelineEntry.title}</h3>
+                                    <span>{timelineEntry.location}</span>
+                                </div>
+                                <div className="content"><p>{timelineEntry.description}</p></div>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
+            </div>
         </Section>
+
     )
 }
 
--- src/components/page-components/Work/index.js
+++ src/components/page-components/Work/index.js
@@ -3,9 +3,34 @@ import Section from '../../base/Section'
 import './index.css';
 
 const Work = (props) => {
+    const timelineEntries = [
+        {"timeline": "2022*", "title": "AEM Technology Specialist / Architect", "location": "Evolutyz Corp., USA", "description": "Architected & delivered highly secured, scalable application"},
+        {"timeline": "2016 - 2022", "title": "AEM Technology Specialist / Architect", "location": "Cognizant Technology Solutions, USA", "description": "Designed & delivered highly secured, scalable & dynamic applications."},
+        {"timeline": "2014 - 2016", "title": "Sr. Consultant", "location": "Client: Novartis, Employer: Accenture, INDIA", "description": "Designed & developed Extensive Dynamic implementation for Warehouse Management"},
+        {"timeline": "2010 - 2014", "title": "Sr. Consultant", "location": "Client: John Deere, Employer: Yash Technologies Pvt. Ltd., INDIA", "description": "Designed & developed many scalable & dynamic implementations to support content migration"}
+    ]
+
     return (
         <Section {...props}>
-            <p>Work Section</p>
+            <div className="section-title">
+                <h2>Work</h2>
+            </div>
+            <div className="work-timeline">
+                <ul className="timeline">
+                    { timelineEntries && timelineEntries.map((timelineEntry, index) => (
+                        <li key={timelineEntry.timeline} className={(index % 2) == 1 ? 'timeline-inverted': ''}>
+                            <div className="timeline-info">{timelineEntry.timeline}</div>
+                            <div className="timeline-panel">
+                                <div className="heading">
+                                    <h3>{timelineEntry.title}</h3>
+                                    <span>{timelineEntry.location}</span>
+                                </div>
+                                <div className="content"><p>{timelineEntry.description}</p></div>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
+            </div>
         </Section>
     )
 }
